b0092ac30597a2124f64e6b4018e6fc73a272377
Zain <zmathews@student.wethinkcode.co.za>
2020-05-24 13:55:46 +0200

    space

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -51,7 +51,7 @@ int		is_flag(char *str);
 t_ls	*store_type_name(char *path);
 t_ls	*no_options(char *path);
 void	sort_a(t_ls *tmp);
-void	print_a(t_ls  *list);
+void	print_a(t_ls *list);
 void	print_base(t_ls *list);
 void	sort_r(t_ls *tmp);
 t_ls	*store_recursive(char *path);
